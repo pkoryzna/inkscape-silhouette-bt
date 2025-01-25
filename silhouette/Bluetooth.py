202b9f331043cd3e149f135f525d10eb2340c449
Patryk Koryzna <patryk@koryzna.com>
2025-01-25 16:27:01 +0100

    BLE: retry on timeouts, close connection

--- silhouette/Bluetooth.py
+++ silhouette/Bluetooth.py
@@ -7,6 +7,8 @@ import socket
 from silhouette.DeviceConstants import *
 from silhouette.connection import SilhouetteCameoConnection
 
+MAX_RETRY_LIMIT = 5
+
 PORT_RANGE = range(2000, 20000)
 
 # UUID of service that emulates a serial port/UART over BLE
@@ -44,13 +46,18 @@ class SilhouetteBleSerialConnection(SilhouetteCameoConnection):
     Depending on the OS, you will see an UUID (macOS) or a MAC address for the device ID.
 
     """
-    def __init__(self, device_id=None, progress_cb=None):
+
+    def __init__(
+        self, device_id=None, force_hardware=None, progress_cb=None, log=sys.stderr
+    ):
+        self.sock = None
         self.port = None
         self.ble_serial_proc = None
         self._attempt_connect(device_id)
         # TODO autodetect based on GATT attributes
-        self.hardware = None
+        self.hardware = force_hardware
         self.progress_cb = progress_cb
+        self.log = log
 
     def _attempt_connect(self, device_id, max_retries=10):
         """Run ble_serial on a random TCP port and connect"""
@@ -72,37 +79,46 @@ class SilhouetteBleSerialConnection(SilhouetteCameoConnection):
             ]
 
             if device_id:
-                ble_serial_args.append('-d')
+                ble_serial_args.append("-d")
                 ble_serial_args.append(device_id)
 
-            print(f"opening ble_serial: {ble_serial_args}", file=sys.stderr, flush=True)
+            print(f"opening ble_serial: {ble_serial_args}", file=self.log, flush=True)
 
             self.ble_serial_proc = subprocess.Popen(ble_serial_args)
             time.sleep(6.0)
-            print(f"checking ble_serial status", file=sys.stderr)
+            print(f"checking ble_serial status", file=self.log)
             self.ble_serial_proc.poll()
             if self.ble_serial_proc.returncode is not None:
+                print(
+                    f"failed to connect on port {port} "
+                    f"to ble_serial pid {self.ble_serial_proc.pid}",
+                    file=self.log,
+                )
                 self.ble_serial_proc = None
             else:
-                print(f"connecting to ble_serial TCP server on {port}", file=sys.stderr)
+                print(f"connecting to ble_serial TCP server on {port}", file=self.log)
                 self.sock = socket.socket()
                 self.sock.settimeout(10.0)
                 self.sock.connect(("localhost", port))
                 self.port = port
+                print("successfully connected")
                 break
         if self.ble_serial_proc is None:
             raise Exception("Failed to connect with ble_serial after 10 attempts")
 
-    def __del__(self):
+    def close(self):
         if self.sock:
             self.sock.close()
+            self.sock = None
         if self.ble_serial_proc:
             self.ble_serial_proc.kill()
+            self.ble_serial_proc = None
+
 
     def read(self, size=64, timeout=5000):
         if not self.sock:
             raise Exception("Not connected to ble_serial server")
-        self.sock.settimeout(timeout/1000.0)
+        self.sock.settimeout(timeout / 1000.0)
         return self.sock.recv(size)
 
     def write(self, data, is_query=False, timeout=10000):
@@ -113,18 +129,37 @@ class SilhouetteBleSerialConnection(SilhouetteCameoConnection):
         try:
             resp = self.read(timeout=10)  # poll the inbound buffer
             if resp:
-                print("response before write('%s'): '%s'" % (data, resp), sys.stderr)
-        except:
+                print("response before write('%s'): '%s'" % (data, resp), file=sys.stderr)
+        except socket.timeout:
             pass
 
-
-        self.sock.settimeout(timeout/1000.0)
+        self.sock.settimeout(timeout / 1000.0)
         sent_total = 0
         chunk_size = 4096
+        retries = 0
+        last_exc = None
         while sent_total < len(data):
-            chunk = data[sent_total:sent_total+chunk_size]
-            sent = self.sock.send(chunk)
-            # TODO handle non-fatal timeouts like in usb
-            self.progress_cb(sent_total, len(data), '')
-            sent_total =+ sent
+            if retries > MAX_RETRY_LIMIT:
+                e = Exception(f"Write failed after {MAX_RETRY_LIMIT} retries")
+                if last_exc:
+                    raise e from last_exc
+                raise e
+            try:
+                chunk = data[sent_total : sent_total + chunk_size]
+                sent = self.sock.send(chunk)
+                sent_total = +sent
+                self.progress_cb(sent_total, len(data), "")
+            except socket.timeout as e:
+                print("socket write timed out, waiting 1 sec and retrying",file=self.log)
+                time.sleep(1)
+                retries += 1
+                last_exc = e
+                continue
+            if sent == 0:
+                time.sleep(1)
+                retries += 1
+                last_exc = None
+            else:
+                retries = 0
+                last_exc = None
 
